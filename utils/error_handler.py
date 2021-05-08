1d2ca19ef05339044c67f17fedbc90680dd4f07f
Hayden Hook <id626493@outlook.com>
2021-05-09 00:46:57 +0100

    Improve Error Handling

--- utils/error_handler.py
+++ utils/error_handler.py
@@ -1,5 +1,6 @@
 from discord import Embed
 from discord.ext import commands
+from discord.ext.commands.errors import CommandError
 
 def embed_error(message):
     return Embed(
@@ -7,8 +8,7 @@ def embed_error(message):
         color=0xFF0000
     )
 
-
-class MissingArgument(Exception):
+class MissingArgument(commands.CommandError):
     def __init__(self, missing_argument, command_description, message="Missing keyword: ", ):
         self.missing_argument = f"`{missing_argument}`"
         self.command_description = f"`{command_description}`"
@@ -16,18 +16,12 @@ class MissingArgument(Exception):
         self.missing_argument += f"\nCommand Usage: {self.command_description}"
         self.message = message + self.missing_argument
 
-    def __str__(self):
-        return self.message
-
-class MissingPermissionOnMember(Exception):
+class MissingPermissionOnMember(commands.CommandError):
     def __init__(self, command, member, message="I have no permissions to", ):
         self.command = f"`{command}`"
         self.member = f"{member.mention}"
 
         self.message = f"{message} use {self.command} on {self.member}"
-
-    def __str__(self):
-        return self.message
 
 class CommandErrorHandler(commands.Cog):
 
@@ -36,30 +30,19 @@ class CommandErrorHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        print(error)
+        #print(str(error))
+
+        if isinstance(type(error), type(MissingArgument)):
+            embed = embed_error(str(error))
 
         if isinstance(error, commands.CommandNotFound):
             unfound_command = str(error).split(" ")[1][1:-1]
             embed = embed_error(f"Command `{unfound_command}` is unrecognised.")
 
-        if isinstance(error, commands.BadArgument):
-            if "Member" in str(error) and "not found" in str(error):
-            user = str(error).split(" ")[1]
+        if isinstance(error, commands.MemberNotFound):
+            user = str(error).split(" ")[1][1:-1]
             embed = embed_error(f"Member `{user}` not found") 
-        
-        elif "MissingPermissionOnMember" in str(error):
-            error = str(error)[len("Command raised an exception: MissingPermissionOnMember: "):]
-            embed = embed_error(error)
 
-        else:
-            try:
-                error = str(error).split(":")
-                error = error[2] + ": " + error[3] + ": " + error[4]
-                embed = embed_error(error)
-            except:
-                error = f"Unkown Error Occured! : {error}"
-                embed = embed_error(error)
-            
         await ctx.send(embed=embed)
 
         
