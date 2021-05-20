87ccc1c973443ea38a3e4893cdb7af6b185f6316
Hayden Hook <id626493@outlook.com>
2021-05-20 17:40:58 +0100

    Implement database

--- Website/website.py
+++ Website/website.py
@@ -1,7 +1,7 @@
 from quart import Quart, render_template, request, session, redirect, url_for
 from quart_discord import DiscordOAuth2Session
 from discord.ext import ipc
-from discord.ext import commands
+from pymongo import MongoClient
 import discord
 
 app = Quart(__name__)
@@ -15,12 +15,36 @@ with open('Website\protected_website_vars.env') as ins:
         if match is not None:
             result[match.group(1)] = match.group(2)
 
+
 app.config["SECRET_KEY"] = str(result["SECRET_KEY"])
 app.config["DISCORD_CLIENT_ID"] = int(result["DISCORD_CLIENT_ID"])   # Discord client ID.
 app.config["DISCORD_CLIENT_SECRET"] = str(result["DISCORD_CLIENT_SECRET"])   # Discord client secret.
 app.config["DISCORD_REDIRECT_URI"] = str(result["DISCORD_REDIRECT_URI"])
 
+CLUSTER = MongoClient(result["SRV_URL"])
+CLUSTERS = {
+    "CLUSTER_EXPERIENCE" : "leveling",
+    "CLUSTER_RATELIMIT" : "xp_rate_limit",
+    "CLUSTER_AFK" : "afk",
+    "CLUSTER_GAY" : "gay",
+    "CLUSTER_DICK" : "dick",
+    "CLUSTER_PUSSY" : "pussy",
+    "CLUSTER_SHIP" : "ship",
+    "CLUSTER_MUTE" : "mute",
+    "CLUSTER_SERVER_ROLES" : "utils",
+    "CLUSTER_BLACKLIST_WORDS" : "utils",
+    "CLUSTER_GIFS" : "utils",
+    "CLUSTER_CONFESSION" : "utils",
+    "CLUSTER_CHANNELS" : "utils",
+}
 
+
+def get_cluster(guild, cluster, clusters = CLUSTERS):
+    val = clusters.get(cluster)
+    return CLUSTER[str(guild)][val]
+
+def get_channel_id(guild_id, channel_name):
+    return get_cluster(guild_id, "CLUSTER_CHANNELS").find_one({"id" : "type_important_channels"})["dict"][channel_name]
 discord = DiscordOAuth2Session(app)
 
 @app.route("/")
@@ -66,13 +90,14 @@ async def dashboard_server(guild_id):
 		return redirect(url_for("login")) 
 
 	guild = await ipc_client.request("get_guild", guild_id = guild_id)
-	print(guild["name"])
-	
+
+	_db_important_channels = get_cluster(guild_id, "CLUSTER_CHANNELS").find_one({"id" : "type_important_channels"})["dict"]
+
 	if guild is None:
 		return redirect(f'https://discord.com/oauth2/authorize?&client_id={app.config["DISCORD_CLIENT_ID"]}&scope=bot&permissions=8&guild_id={guild_id}&response_type=code&redirect_uri={app.config["DISCORD_REDIRECT_URI"]}')
 		
 	return await render_template(
-		"guild_id.html", guild=guild, 
+		"guild_id.html", guild=guild, _db_important_channels=_db_important_channels
 	)
 
 
